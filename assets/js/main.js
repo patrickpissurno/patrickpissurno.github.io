84f4dce7c4ade4e33c42e2d572516e98752f2c07
Patrick Pissurno <patrickpissurno.nave@gmail.com>
2016-06-10 16:05:07 -0300

    HTTPS testings

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,10 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+var host = "patrickpissurno.com.br";
+if ((host == window.location.host) && (window.location.protocol != "https:"))
+    window.location.protocol = "https";
+
 (function($) {
 
 	skel.breakpoints({
